43b2a874e2d1a7f87f52a1488069707ce61d6d64
Magda <meg.nowak27@gmail.com>
2022-02-20 15:22:53 +0100

    create repo

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const sum = require("./sum");
+import style from "./scss/index.scss";
+
+
+console.log("hola")
+// import {
+//     sum2
+// } from "./sum2"
+// import Icon from "./assets/img/children.jpg";
+
+// console.log("Hello World!");
+// console.log(sum.sum(2, 4));
+// console.log(sum2(20, 40));
+
+// const div = document.getElementById('demo');
+// const sumValue = sum.sum(10, 65);
+
+// div.innerHTML = `10 = ${sumValue}`;
+
+// let myIcon = new Image();
+// myIcon.src = Icon;
+// console.log(myIcon.src);
+
+// document.querySelector('#photo').appendChild(myIcon);
